b7ff51d393bd1d75ff32392771e0e26bec30d1d5
Suraj Agrawal <surajagwl123@gmail.com>
2026-08-30 01:28:52 +0530

    connection.py: MongoDB client setup and DB handle

---
+++ database/connection.py
@@ -0,0 +1,11 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("DB_NAME")]
+
+def get_db():
+    return db
