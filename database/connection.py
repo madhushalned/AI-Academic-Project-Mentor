20ae54cf024a5ba2f378ffcba18e3eae33cd9924
Suraj Agrawal <surajagwl123@gmail.com>
2026-08-30 01:50:23 +0530

    connection.py: fix SSL certificate verification using certifi

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,13 @@
 import os
+import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
-client = MongoClient(os.getenv("MONGO_URI"))
+client = MongoClient(os.getenv("MONGO_URI"), tlsCAFile=certifi.where())
 db = client[os.getenv("DB_NAME")]
 
 def get_db():
