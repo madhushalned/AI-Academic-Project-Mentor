abb3197994b06eb3d2d96a5414d836869380141e
Madhushalne <madhushalne@gmail.com>
2026-08-30 15:23:23 +0530

    Implement student API with MongoDB

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
+from app.routes.student_routes import router as student_router
+
 app = FastAPI()
+
+app.include_router(student_router)
 
 
 @app.get("/")
 def home():
-    return {
-        "message": "AI Academic Mentor Backend Running"
-    }
+    return {"message": "AI Academic Project Mentor API is running"}
